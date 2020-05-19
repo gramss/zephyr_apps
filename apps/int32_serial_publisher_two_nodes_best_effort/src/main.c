b496074a1047a467277fe29769717be82efa264e
Florian Gramß <flo.gramss@gmail.com>
2020-05-19 10:00:24 +0200

    altered to only 1 rcl_init and better cleanup

--- apps/int32_serial_publisher_two_nodes_best_effort/src/main.c
+++ apps/int32_serial_publisher_two_nodes_best_effort/src/main.c
@@ -22,10 +22,11 @@ void main(void)
 	rmw_init_options_t* rmw_options = rcl_init_options_get_rmw_init_options(&options);
 	RCCHECK(rmw_uros_options_set_client_key(0xDEADBEEF, rmw_options))
 	
-	//node 1
+	//rcl init
 	rcl_context_t context = rcl_get_zero_initialized_context();
 	RCCHECK(rcl_init(0, NULL, &options, &context))
 
+	//node 1
 	rcl_node_options_t node_ops = rcl_node_get_default_options();
 
 	rcl_node_t node = rcl_get_zero_initialized_node();
@@ -33,13 +34,11 @@ void main(void)
 
 
 	//node 2
-	rcl_context_t context2 = rcl_get_zero_initialized_context();
-	RCCHECK(rcl_init(0, NULL, &options, &context2))
 
 	rcl_node_options_t node_ops2 = rcl_node_get_default_options();
 
 	rcl_node_t node2 = rcl_get_zero_initialized_node();
-	RCCHECK(rcl_node_init(&node2, "node2", "", &context2, &node_ops2))
+	RCCHECK(rcl_node_init(&node2, "node2", "", &context, &node_ops2))
 
 	//publisher 1
 	rcl_publisher_options_t publisher_ops = rcl_publisher_get_default_options();
@@ -70,7 +69,8 @@ void main(void)
 	} while (true);
 
 	RCCHECK(rcl_publisher_fini(&publisher, &node))
-	RCCHECK(rcl_publisher_fini(&publisher2, &node))
+	RCCHECK(rcl_publisher_fini(&publisher2, &node2))
 
 	RCCHECK(rcl_node_fini(&node))
+	RCCHECK(rcl_node_fini(&node2))
 }
